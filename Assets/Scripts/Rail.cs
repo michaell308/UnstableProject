5315cafc98620ca66ad13df512c8178b0ccfa9de
michaell308 <michaell308@yahoo.com>
2021-10-02 14:32:08 -0500

    added rail prefabs

--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Rail : MonoBehaviour
 {
-    private float speed = 10;
+    private float speed = 500;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Vector3 moveVector = new Vector3(45, 45, 0);
-        //rb.AddForce(-transform.right * speed * Time.deltaTime, ForceMode2D.Impulse);
-        rb.AddForce(-moveVector * speed * Time.deltaTime, ForceMode2D.Impulse);
+        rb.AddForce(-transform.right * speed * Time.deltaTime, ForceMode2D.Impulse);
+        //rb.AddForce(-moveVector * speed * Time.deltaTime, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
